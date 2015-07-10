0791f9ad4ee620362a621bd1c0e4bc01e361a97e
jacobjonesgw <jacob.theodore.jones@gmail.com>
2015-07-10 11:13:03 -0400

    Initial Commit
    Uploading the base files for the project.

---
+++ src/FrameManager.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class FrameManager 
+{
+	
+	public FrameManager()
+	{
+		
+	}
+	
+}
---
+++ src/Starter.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Starter 
+{
+
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
