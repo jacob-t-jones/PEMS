5df392dea9da1b28675adc080f3000d308a95418
jacobjonesgw <jacob.theodore.jones@gmail.com>
2015-07-10 13:23:16 -0400

    Commenting
    Added documentation comments to the beginning of the class files.

--- src/FrameManager.java
+++ src/FrameManager.java
@@ -1,3 +1,7 @@
+// PEMS (Police Evidence Management System) Version 0.1
+// Copyright 2015 - Jacob Jones and Andrew Rottier
+// FrameManager.java
+
 import java.awt.*;
 import javax.swing.*;
 
@@ -23,6 +27,5 @@ public class FrameManager
 		this.mainFrame.getContentPane().repaint();
 		this.mainFrame.setTitle(title);
 	}
-	
-	
+
 }
