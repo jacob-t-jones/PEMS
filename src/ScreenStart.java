59d2ac65932c292b9935826da4b63e926eef852a
Andrew Rottier <andrewrottier95@gmail.com>
2015-07-10 13:51:59 -0400

    ScreenStart
    ScreenStart class

--- src/ScreenStart.java
+++ src/ScreenStart.java
@@ -9,10 +9,19 @@ public class ScreenStart extends JPanel
 {
 	
 	private FrameManager manager;
+	private JLabel instructions;
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	int width = (int)screenSize.getWidth();
+	int height = (int)screenSize.getHeight();
+
 	
 	public ScreenStart(FrameManager manager)
 	{
 		this.manager = manager;
+		this.instructions = new JLabel();
+		this.instructions.setText("Police Evidence Management System");
+		this.instructions.setBounds(width/2, height/2, width, height);
+		this.add(this.instructions);
 	}
 	
 }
