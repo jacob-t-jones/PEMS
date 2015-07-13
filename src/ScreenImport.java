acca1a0852c845da82e199618249753c31d4d2c2
jacobjonesgw <jacob.theodore.jones@gmail.com>
2015-07-13 13:07:23 -0400

    Image Importation
    Finished getImages() method.

--- src/ScreenImport.java
+++ src/ScreenImport.java
@@ -13,62 +13,39 @@ public class ScreenImport extends JPanel
 {
 	
 	private FrameManager manager;
-	private LinkedList<JLabel> imageLabels;
+	private LinkedList<BufferedImage> images;
+	private String imageDirectoryName;
 	
 	public ScreenImport(FrameManager manager)
 	{
 		this.manager = manager;
-		this.imageLabels = this.getImages();
+		this.images = this.getImages();
 	} 
 	
 	/* getImages - creates "photoLists" and adds images to the ScreenImport
 	 */
-	private LinkedList<JLabel> getImages()
+	private LinkedList<BufferedImage> getImages()
 	{
-		LinkedList<JLabel> photoList = new LinkedList<JLabel>();
-		BufferedImage logoImage = null;
-		
-	    try 
-	    {   
-	    	//for loop to add all of the photos to the photoList..
-	    	
-	    	//replace with path of files on the camera when plugged in
-	    	logoImage = ImageIO.read(new File("resources/logo.png"));
-	    } 
-	    catch (Exception e)
-	    {
-	    	//otherwise add the gif image prompting the user to plug in the camera
-			System.out.println("Please connected the camera via USB");
-			return null;
-	    }
-	    this.logoLabel = new JLabel(new ImageIcon(this.manager.resizeImage(logoImage, 200, 200)));
-	    this.logoLabel.setAlignmentX(CENTER_ALIGNMENT);
-	    this.add(this.logoLabel);
-	    //add images to a list to return
-	    return photoList;
+		File imageDirectory = new File(this.imageDirectoryName);
+		String[] imageFileNames = imageDirectory.list();
+		LinkedList<BufferedImage> imageList = new LinkedList<BufferedImage>();
+		BufferedImage currentImage = null;
+		for (int i = 0; i < imageFileNames.length; i++)
+		{
+		    try 
+		    {   
+		    	currentImage = ImageIO.read(new File(imageDirectory + imageFileNames[i]));
+		    } 
+		    catch (Exception e)
+		    {
+		    	System.out.println("Error - Please connect the camera via USB");
+		    	return null;
+		    }
+		    imageList.addLast(currentImage);
+		}
+	    return imageList;
 	}
 	
-	/* constructTitleLabel - creates "titleLabel" and adds it to ScreenStart
-	 */
-	private void constructTitleLabel()
-	{
-		this.titleLabel = new JLabel("Police Evidence Management System");
-		this.titleLabel.setFont(this.manager.TITLE_FONT);
-		this.titleLabel.setForeground(this.manager.TITLE_COLOR);
-	    this.titleLabel.setAlignmentX(CENTER_ALIGNMENT);
-		this.add(this.titleLabel);
-	}
-	
-	/* constructNameLabel - creates "nameLabel" and adds it to ScreenStart
-	 */
-	private void constructNameLabel()
-	{
-		this.nameLabel = new JLabel(this.manager.getConfiguration().getDepartmentName());
-		this.nameLabel.setFont(this.manager.SUBTITLE_FONT);
-		this.nameLabel.setForeground(this.manager.SUBTITLE_COLOR);
-	    this.nameLabel.setAlignmentX(CENTER_ALIGNMENT);
-		this.add(this.nameLabel);
-	}
 
 }
 
