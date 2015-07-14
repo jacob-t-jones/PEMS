5546ae06699f415bb82fe87166aa84b845896d31
Andrew Rottier <andrewrottier95@gmail.com>
2015-07-14 10:47:52 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	bin/Config.class
    	bin/FrameManager.class
    	bin/ScreenImport.class
    	bin/ScreenNewCase$1.class
    	bin/ScreenNewCase$2.class
    	bin/ScreenNewCase.class
    	src/ScreenImport.java

--- src/ScreenImport.java
+++ src/ScreenImport.java
@@ -3,46 +3,59 @@
 //ScreenImport.java
 
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import com.sun.glass.events.MouseEvent;
 
 public class ScreenImport extends JPanel
 {
 
 	private FrameManager manager;
-<<<<<<< HEAD
-	private BufferedImage[] images;
-	private JLabel[] labels;
-	//private Box[] rows;
-	//private Box[] col;
-	
 	private String imageDirectoryName;
 	private JLabel instructionsLabel;
+<<<<<<< HEAD
 	private JButton continueButton;
 =======
+>>>>>>> origin/master
 	private ImageEditor imgEditor;
 	private ArrayList<BufferedImage> images;
 	private ArrayList<JLabel> labels;
 	private String directoryName;
 	private Box imgBox;
+	private Box selBox; //selected box
 	private Box buttonsBox;
 	private JButton nextButton;
 	private JButton prevButton;
 	private int imagePlace;
+<<<<<<< HEAD
+	private int selectedImagePlace;
+	
+	private ArrayList<JButton> buttons;
+	private ArrayList<JLabel> selected;
+=======
 >>>>>>> origin/master
 
 	public ScreenImport(FrameManager manager)
 	{
 		this.manager = manager;
 		this.imgEditor = new ImageEditor();
-		this.directoryName = "/Users/Jacob/Documents/Pics";
-		this.imagePlace = 0;
+		this.directoryName = "/Users/andrewrottier/Documents/Pictures/SamplePictures";
+		this.imagePlace = 0; this.selectedImagePlace = 0;
 		this.images = this.getImages();
-		this.labels = this.fillLabels(); 
+		this.labels = this.fillLabels();
+		this.selBox = Box.createHorizontalBox();
+		this.selBox.setBorder(BorderFactory.createLineBorder(Color.black));
+		this.imgBox = Box.createVerticalBox();
+		this.imgBox.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.displayImages();
+		this.selected = new ArrayList<JLabel>();
+		this.addActions();
 		this.populateButtonsBox();
 		this.manager.setResizable(true);
 		this.manager.maximizeFrame();
@@ -80,32 +93,112 @@ public class ScreenImport extends JPanel
 		ArrayList<JLabel> labelList = new ArrayList<JLabel>();
 		for (int i = 0; i < this.images.size(); i++)
 		{
-			labelList.add(new JLabel(new ImageIcon(this.imgEditor.resizeImage(this.images.get(i), 150, 200))));
+			JLabel newLabel = new JLabel(new ImageIcon(this.imgEditor.resizeImage(this.images.get(i), 150, 200)));
+			newLabel.setAlignmentX(CENTER_ALIGNMENT);
+			labelList.add(newLabel);
 		}
 		return labelList;
 	}
 	
 	/* fillRows - fills a new row with 5 new images from our labels list
-	 * *****after put all code inside a for loop to create 3 rows******
+	 * *****check to see how many images there are ******
 	 */
 	private void displayImages()
 	{
-		this.imgBox = Box.createVerticalBox();
+		//add selected images to the screen
+		selectedImagePlace = 0;
+		
+		for(int i = 0; i < 15; i++){
+			try
+			{
+				this.imgBox.setAlignmentX(CENTER_ALIGNMENT);
+				this.selBox.setAlignmentX(CENTER_ALIGNMENT);
+				selBox.add(this.selected.get(this.selectedImagePlace));
+				//selBox.add(Box.createHorizontalStrut(25));
+				selectedImagePlace++;
+			}
+			catch(Exception e){} 
+			
+		}
+		this.add(this.selBox);
+		
+		//add the images on the camera to the screen
+		imagePlace = 0;
+		
 		for (int i = 0; i < 3; i++)
 		{
 			Box row = Box.createHorizontalBox();
 			for (int j = 0; j < 5; j++)
 			{
-				row.add(this.labels.get(this.imagePlace));
-				row.add(Box.createHorizontalStrut(25));
-				imagePlace++;
+				try
+				{
+					row.add(this.labels.get(this.imagePlace));
+					imagePlace++;
+				}
+				catch (Exception e)
+				{
+					row.add(Box.createHorizontalStrut(150)); //space the size of a pic
+					System.out.println("No more pictures");//remove
+				}
+				row.add(Box.createHorizontalStrut(25)); //spacing between pictures
+				
 			}
-			this.imgBox.add(Box.createVerticalStrut(10));
+			row.setAlignmentX(CENTER_ALIGNMENT);
 			this.imgBox.add(row);
-			this.imgBox.add(Box.createVerticalStrut(10));
 		}
 		this.add(this.imgBox);
+		this.add(this.selBox);
+		revalidate();
+		repaint();
 		return;
+	}
+	
+	
+	/* addActions - turns each picture into a button
+	 */
+	private void addActions(){
+		
+		for(int i = 0; i < labels.size(); i++)
+		{
+			JLabel currentLabel = this.labels.get(i);
+			currentLabel.addMouseListener(new MouseListener()
+			{
+				@Override
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					System.out.println("DO THIS");
+					selected.add(currentLabel);
+					labels.remove(currentLabel);
+					displayImages();
+					
+				}
+
+				@Override
+				public void mousePressed(java.awt.event.MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseReleased(java.awt.event.MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseEntered(java.awt.event.MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseExited(java.awt.event.MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				
+			});
+		}
 	}
 	
 	private void populateButtonsBox()
