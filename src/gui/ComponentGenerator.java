f11288d5baaa75f258ebe7df0a6a7707df8224be
jacobjonesgw <jacob.theodore.jones@gmail.com>
2015-07-15 13:47:50 -0400

    ComponentGenerator
    Began creating a generic class to handle the creation of commonly used
    components.

---
+++ src/gui/ComponentGenerator.java
@@ -0,0 +1,136 @@
+// PEMS (Police Evidence Management System) Version 0.1
+// Copyright 2015 - Jacob Jones and Andrew Rottier
+// ComponentGenerator.java
+
+package gui;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
+
+public class ComponentGenerator 
+{
+	
+	public ComponentGenerator()
+	{
+		
+	}
+	
+	/* generateButton - creates and returns a JButton that complies with the parameters
+	 *           text - the text displayed in the button
+	 *         action - the action to execute when the button is pressed
+	 */
+	public static JButton generateButton(String text, ActionListener action)
+	{
+		JButton newButton = new JButton(text);
+		newButton.addActionListener(action);
+		return newButton;
+	}
+	
+	/* generateButton - creates and returns a JButton that complies with the parameters
+	 *           text - the text displayed in the button
+	 *         action - the action to execute when the button is pressed
+	 *     alignmentX - the horizontal alignment of the button
+	 */
+	public static JButton generateButton(String text, ActionListener action, float alignmentX)
+	{
+		JButton newButton = new JButton(text);
+		newButton.addActionListener(action);
+		newButton.setAlignmentX(alignmentX);
+		return newButton;
+	}
+	
+	/* generateButton - creates and returns a JButton that complies with the parameters
+	 *           text - the text displayed in the button
+	 *         action - the action to execute when the button is pressed
+	 *     alignmentX - the horizontal alignment of the button
+	 *     alignmentY - the vertical alignment of the button
+	 */
+	public static JButton generateButton(String text, ActionListener action, float alignmentX, float alignmentY)
+	{
+		JButton newButton = new JButton(text);
+		newButton.addActionListener(action);
+		newButton.setAlignmentX(alignmentX);
+		newButton.setAlignmentY(alignmentY);
+		return newButton;
+	}
+	
+	/* generateLabel - creates and returns a JLabel that complies with the parameters
+	 *         image - the image displayed in the label
+	 */
+	public static JLabel generateLabel(BufferedImage image)
+	{
+		JLabel newLabel = new JLabel(new ImageIcon(image));
+		return newLabel;
+	}
+	
+	/* generateLabel - creates and returns a JLabel that complies with the parameters
+	 *         image - the image displayed in the label
+	 *    alignmentX - the horizontal alignment of the label
+	 */
+	public static JLabel generateLabel(BufferedImage image, float alignmentX)
+	{
+		JLabel newLabel = new JLabel(new ImageIcon(image));
+		newLabel.setAlignmentX(alignmentX);
+		return newLabel;
+	}
+	
+	/* generateLabel - creates and returns a JLabel that complies with the parameters
+	 *         image - the image displayed in the label
+	 *    alignmentX - the horizontal alignment of the label
+	 *    alignmentY - the vertical alignment of the label
+	 */
+	public static JLabel generateLabel(BufferedImage image, float alignmentX, float alignmentY)
+	{
+		JLabel newLabel = new JLabel(new ImageIcon(image));
+		newLabel.setAlignmentX(alignmentX);
+		newLabel.setAlignmentY(alignmentY);
+		return newLabel;
+	}
+	
+	/* generateLabel - creates and returns a JLabel that complies with the parameters
+	 *          text - the text displayed in the label
+	 *          font - the font type to use in the label
+	 *         color - the font color to use in the label
+	 */
+	public static JLabel generateLabel(String text, Font font, Color color)
+	{
+		JLabel newLabel = new JLabel(text);
+		newLabel.setFont(font);
+		newLabel.setForeground(color);
+		return newLabel;
+	}
+	
+	/* generateLabel - creates and returns a JLabel that complies with the parameters
+	 *          text - the text displayed in the label
+	 *          font - the font type to use in the label
+	 *         color - the font color to use in the label
+	 *    alignmentX - the horizontal alignment of the label
+	 */
+	public static JLabel generateLabel(String text, Font font, Color color, float alignmentX)
+	{
+		JLabel newLabel = new JLabel(text);
+		newLabel.setFont(font);
+		newLabel.setForeground(color);
+		newLabel.setAlignmentX(alignmentX);
+		return newLabel;
+	}
+	
+	/* generateLabel - creates and returns a JLabel that complies with the parameters
+	 *          text - the text displayed in the label
+	 *          font - the font type to use in the label
+	 *         color - the font color to use in the label
+	 *    alignmentX - the horizontal alignment of the label
+	 *    alignmentY - the vertical alignment of the label
+	 */
+	public static JLabel generateLabel(String text, Font font, Color color, float alignmentX, float alignmentY)
+	{
+		JLabel newLabel = new JLabel(text);
+		newLabel.setFont(font);
+		newLabel.setForeground(color);
+		newLabel.setAlignmentX(alignmentX);
+		newLabel.setAlignmentY(alignmentY);
+		return newLabel;
+	}
+
+}
