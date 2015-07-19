824dcdc88ae9c58712261e54442b6026e291fbea
jacobjonesgw <jacob.theodore.jones@gmail.com>
2015-07-18 23:40:10 -0400

    Shortcuts, Menu Items
    Added new menu items with keyboard shortcuts.

--- src/gui/panels/ScreenEdit.java
+++ src/gui/panels/ScreenEdit.java
@@ -13,9 +13,14 @@ public class ScreenEdit extends JPanel implements ActionListener
 	private FrameManager manager;
 	private JMenuBar menuBar;
 	private JMenu fileMenu;
+	private JMenu editMenu;
 	private JMenu caseMenu;
 	private JMenu imageMenu;
+	private JMenuItem saveImageMenuItem;
 	private JMenuItem renameImageMenuItem;
+	private JMenuItem quitMenuItem;
+	private JMenuItem undoMenuItem;
+	private JMenuItem redoMenuItem;
 	private JMenuItem removeImageMenuItem;
 	private JMenuItem antiAliasMenuItem;
 	private JMenuItem brightenMenuItem;
@@ -34,9 +39,25 @@ public class ScreenEdit extends JPanel implements ActionListener
 	
 	public void actionPerformed(ActionEvent e)
 	{ 
-		if (e.getSource() == this.renameImageMenuItem)
+		if (e.getSource() == this.saveImageMenuItem)
 		{
 			
+		}
+		else if (e.getSource() == this.renameImageMenuItem)
+		{
+			
+		}
+		else if (e.getSource() == this.quitMenuItem)
+		{
+
+		}
+		else if (e.getSource() == this.undoMenuItem)
+		{
+
+		}
+		else if (e.getSource() == this.redoMenuItem)
+		{
+
 		}
 		else if (e.getSource() == this.removeImageMenuItem)
 		{
@@ -79,9 +100,48 @@ public class ScreenEdit extends JPanel implements ActionListener
 	private void constructMenuBar()
 	{
 		this.menuBar = new JMenuBar();
-		this.fileMenu = new JMenu();
-		this.caseMenu = new JMenu();
-		this.imageMenu = new JMenu();
+		this.fileMenu = new JMenu("File");
+		this.editMenu = new JMenu("Edit");
+		this.caseMenu = new JMenu("Case");
+		this.imageMenu = new JMenu("Image");
+		this.saveImageMenuItem = ComponentGenerator.generateMenuItem("Save Image", this, KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+		this.renameImageMenuItem = ComponentGenerator.generateMenuItem("Rename Image", this);
+		this.quitMenuItem = ComponentGenerator.generateMenuItem("Quit", this, KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+		this.undoMenuItem = ComponentGenerator.generateMenuItem("Undo", this, KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK));
+		this.redoMenuItem = ComponentGenerator.generateMenuItem("Redo", this, KeyStroke.getKeyStroke(KeyEvent.VK_Y, ActionEvent.CTRL_MASK));
+		this.removeImageMenuItem = ComponentGenerator.generateMenuItem("Remove Image from Case", this);
+		this.antiAliasMenuItem = ComponentGenerator.generateMenuItem("Apply Anti Aliasing", this);
+		this.brightenMenuItem = ComponentGenerator.generateMenuItem("Brighten by 10%", this);
+		this.darkenMenuItem = ComponentGenerator.generateMenuItem("Darken by 10%", this);
+		this.grayscaleMenuItem = ComponentGenerator.generateMenuItem("Convert to Grayscale", this);
+		this.resizeMenuItem = ComponentGenerator.generateMenuItem("Resize Image", this);
+		this.rotate90MenuItem = ComponentGenerator.generateMenuItem("Rotate Image 90\u00b0 Right", this);
+		this.rotate180MenuItem = ComponentGenerator.generateMenuItem("Rotate Image 180\u00b0 Right", this);
+		this.rotate270MenuItem = ComponentGenerator.generateMenuItem("Rotate Image 270\u00b0 Right", this);
+		this.fileMenu.add(this.saveImageMenuItem);
+		this.fileMenu.add(this.renameImageMenuItem);
+		this.fileMenu.addSeparator();
+		this.fileMenu.add(this.quitMenuItem);
+		this.editMenu.add(this.undoMenuItem);
+		this.editMenu.add(this.redoMenuItem);
+		this.caseMenu.add(this.removeImageMenuItem);
+		this.imageMenu.add(this.antiAliasMenuItem);
+		this.imageMenu.addSeparator();
+		this.imageMenu.add(this.brightenMenuItem);
+		this.imageMenu.add(this.darkenMenuItem);
+		this.imageMenu.addSeparator();
+		this.imageMenu.add(this.grayscaleMenuItem);
+		this.imageMenu.addSeparator();
+		this.imageMenu.add(this.resizeMenuItem);
+		this.imageMenu.addSeparator();
+		this.imageMenu.add(this.rotate90MenuItem);
+		this.imageMenu.add(this.rotate180MenuItem);
+		this.imageMenu.add(this.rotate270MenuItem);
+		this.menuBar.add(this.fileMenu);
+		this.menuBar.add(this.editMenu);
+		this.menuBar.add(this.caseMenu);
+		this.menuBar.add(this.imageMenu);
+		this.manager.setMenuBar(this.menuBar);
 	}
 
 }
