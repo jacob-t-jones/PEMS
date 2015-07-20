290c4028ae7af81ede60598e6fbe70ae07c7368e
jacobjonesgw <jacob.theodore.jones@gmail.com>
2015-07-19 20:10:43 -0400

    Renamed Starter, Improved Look & Feel

--- src/PEMS.java
+++ src/PEMS.java
@@ -1,14 +1,15 @@
 // PEMS (Police Evidence Management System) Version 0.1
 // Copyright 2015 - Jacob Jones and Andrew Rottier
-// Starter.java
+// PEMS.java
 
 import gui.FrameManager;
 
-public class Starter 
+public class PEMS 
 {
 
 	public static void main(String[] args)
 	{
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
 		FrameManager frame = new FrameManager();
 	}
 	
