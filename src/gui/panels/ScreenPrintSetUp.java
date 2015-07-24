348a7591c9ce7a7a2d6564725570261ef8318525
Andrew Rottier <andrewrottier95@gmail.com>
2015-07-24 15:26:03 -0400

    generate pdf

--- src/gui/panels/ScreenPrintSetUp.java
+++ src/gui/panels/ScreenPrintSetUp.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.Box;
@@ -16,6 +17,14 @@ import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import org.apache.pdfbox.PDFBox;
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class ScreenPrintSetUp extends JPanel implements ActionListener, FocusListener
 {
@@ -42,6 +51,41 @@ public class ScreenPrintSetUp extends JPanel implements ActionListener, FocusLis
 		this.populateButtonsContainer();
 		this.populateMainContainer();
 		this.add(this.mainContainer);
+	}
+	
+	private PDDocument generatePDF() throws IOException{
+		// Create a document and add a page to it
+		PDDocument document = new PDDocument();
+		PDPage page = new PDPage();
+		document.addPage( page );
+
+		// Create a new font object selecting one of the PDF base fonts
+		PDFont font = PDType1Font.HELVETICA_BOLD;
+
+		// Start a new content stream which will "hold" the to be created content
+		PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+		// Define a text content stream using the selected font, moving the cursor and drawing the text "Hello World"
+		contentStream.beginText();
+		contentStream.setFont( font, 12 );
+		contentStream.moveTextPositionByAmount( 100, 700 );
+		contentStream.drawString( "Hello World" );
+		contentStream.endText();
+
+		// Make sure that the content stream is closed:
+		contentStream.close();
+
+		// Save the results and ensure that the document is properly closed:
+		try {
+			document.save( "Hello World.pdf");
+		} catch (COSVisitorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		document.close();
+		
+		return null;
+		
 	}
 	
 	
